b9966433d30663b075eddffcc85eb07fcad7e6b3
Архипов Дмитрий <arhidiman@mail.ru>
2023-07-04 09:34:05 +0700

    Добавлен компонент QuestionCard. Настроено хранилище redux и проверена его работоспособность

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Checkbox from 'antd/es/checkbox/Checkbox';
-import { Pagination, Divider, List, Typography, ConfigProvider, } from 'antd';
+import { Pagination } from 'antd';
 import Page from './components/page/Page';
 import './styles/index.scss';
 
@@ -10,33 +9,13 @@ const onChange = ()=> {
 }
 
 
-const data = [
-    // <Checkbox key={1}>answ 1</Checkbox>,
-    <div key={1}></div>,
-    <Checkbox key={2}>answ 2</Checkbox>,
-    <Checkbox key={3}>answ 3</Checkbox>,
-    <Checkbox key={4}>answ 4</Checkbox>
-]
-
 
 export function App() {
     return (
-
-        <ConfigProvider
-            theme={{
-                components: {
-                    Pagination: {
-                        paddingContentHorizontal: 30
-                    }
-                },
-            }}
-        >
-            <div className='app'>
-                <div className='header'>Тест</div>
-                <Page/>
-                <Pagination onChange={onChange} defaultCurrent={1} total={30}/>
-            </div>
-        </ConfigProvider>
-   
+        <div className='app'>
+            <div className='header'>Тест</div>
+            <Page/>
+            <Pagination onChange={onChange} defaultCurrent={1} total={50}/>
+        </div>
     );
 }
--- src/app/components/page/Page.tsx
+++ src/app/components/page/Page.tsx
@@ -1,41 +1,20 @@
-import Checkbox from 'antd/es/checkbox/Checkbox';
-import { Card, Space, Radio } from 'antd';
+import { Card, Space, Radio, Button } from 'antd';
+import QuestionCard from '../QuestionCard/QuestionCard';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { QuestionsState } from 'app/store/app-reducer/questions';
+import { AppState } from 'index';
+
+
+
 
 
 
 const Page = ()=> {
+    const questions = useSelector((state: AppState)=> state.questions)
     return(
         <Space direction='vertical'>
-            <Card title={'Question x'}>
-                <Radio.Group value={1}>
-                    <Space direction='vertical'>
-                        <Radio value={1}>answ 1</Radio>
-                        <Radio value={2}>answ 2</Radio>
-                        <Radio value={3}>answ 3</Radio>
-                        <Radio value={4}>answ 4</Radio>
-                    </Space>
-                </Radio.Group>
-            </Card>
-            <Card title={'Question x'}>
-                <Radio.Group value={1}>
-                    <Space direction='vertical'>
-                        <Radio value={1}>answ 1</Radio>
-                        <Radio value={2}>answ 2</Radio>
-                        <Radio value={3}>answ 3</Radio>
-                        <Radio value={4}>answ 4</Radio>
-                    </Space>
-                </Radio.Group>
-            </Card>
-            <Card title={'Question x'}>
-                <Radio.Group value={1}>
-                    <Space direction='vertical'>
-                        <Radio value={1}>answ 1</Radio>
-                        <Radio value={2}>answ 2</Radio>
-                        <Radio value={3}>answ 3</Radio>
-                        <Radio value={4}>answ 4</Radio>
-                    </Space>
-                </Radio.Group>
-            </Card>
+            {questions.map(question=><QuestionCard key={1} question={question.question}/>)}
+            <Button>Ответить на вопросы</Button>
         </Space>
     )
 }
